0ea16768f07b9d80c24a11e22cf47e74aadb7e7c
Maxime Mato <maxime@maximes-mbp.home>
2025-03-18 11:00:21 +0100

    ajouter un bouton vendre

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -151,6 +151,12 @@ const Navbar = () => {
                 <User className="h-6 w-6" />
               </button>
               
+              <button 
+                onClick={() => navigate('/create-product')}
+                className="text-gray-600 hover:text-gray-900"
+              >
+                Vendre
+              </button>
               {isAuthenticated && (
                 <button 
                   onClick={handleLogout}
